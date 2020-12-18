8d2b3747c1e00a4187b53833e64a4333a7dbe5d3
Csaba_Lehóczky <lcs000x@gmail.com>
2020-12-18 10:18:16 +0100

    2.verzió

--- Main.java
+++ Main.java
@@ -4,11 +4,12 @@ public  class Main
     {
         String abc = "([a-z])([A-Z]+)";
         String rename = "$1_$2";
+        String multiParadigma="javaScript";
         String[] Lista={"camelCase","snakeCase","multiParadigma"};
         for(int i = 0; i<Lista.length;i++)
         {
         System.out.println(Lista[i].replaceAll(abc, rename).toLowerCase());
         }
-    
+        System.out.println(multiParadigma.replaceAll(abc, rename).toLowerCase());
     }
 }
